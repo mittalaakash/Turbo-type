1761580379f032976b54ac97e50758b35f61ba1d
mittalaakash <mit.ash.619@gmail.com>
2021-07-29 22:04:19 +0530

    Initial setup

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <h1>This is a typing application</h1>
+        );
+    }
+}
+export default App;
