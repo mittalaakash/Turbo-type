22c1a4c6768a45a945a9a50dd14f70ecf109b110
mittalaakash <mit.ash.619@gmail.com>
2021-08-01 02:31:50 +0530

    bug nav

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -4,22 +4,24 @@ import logo from '../../assets/turbo-logo.png';
 
 const Nav = () => {
   return (
-    <nav className='nav-container'>
-      <div className='nav-left'>
-        <img src={logo} alt='logo' className='turbo-logo' />
-        <p className='turbo-logo-text'>TurboType</p>
-      </div>
-      <div className='nav-right'>
-        <a
-          target='_blank'
-          rel='noreferrer'
-          className='nav-link'
-          href='https://github.com/mittalaakash'
-        >
-          GitHub
-        </a>
-      </div>
-    </nav>
+    <>
+      <nav className='nav-container'>
+        <div className='nav-left'>
+          <img src={logo} alt='logo' className='turbo-logo' />
+          <p className='turbo-logo-text'>TurboType</p>
+        </div>
+        <div className='nav-right'>
+          <a
+            target='_blank'
+            rel='noreferrer'
+            className='nav-link'
+            href='https://github.com/mittalaakash'
+          >
+            GitHub
+          </a>
+        </div>
+      </nav>
+    </>
   );
 };
 
